119a0802ec66271ce471f9444816019b2ffdaf99
miko866 <mdurik2@gmail.com>
2018-11-26 02:56:18 +0100

    Simple Crypto Web with Django and Bootstrap4

---
+++ crypto_currency_web/virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/miko866/Desktop/Python/Django/crypto_currency_web/virtualenv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
